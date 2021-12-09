ea7d721689aac77fbc42051fc636304bda7e522e
DYNXTSX <francois.peralde@gmail.com>
2021-12-09 21:37:46 +0100

    creation de projet

---
+++ bluetooth.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+#include "bluetooth.h"
+
+void fglobale(void)
+{
+    printf("ceci est un test");
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "bluetooth.h"
+#include <stdio.h>
+
+int main(void)
+{
+    fglobale();
+    return 0;
+}
